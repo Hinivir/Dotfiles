3005e666b556b56b820c67297d78814e39278ed4
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-05-27 16:14:07 +0200

    feat: try to do a personal nvf config

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -1,33 +1,127 @@
 {
-  config,
   inputs,
+  config,
   lib,
+  pkgs,
   ...
-}: let
-  inherit (builtins) filter map toString elem;
-  inherit (lib.filesystem) listFilesRecursive;
-  inherit (lib.strings) hasSuffix;
-  inherit (lib.lists) concatLists;
-  cfg = config.neovim;
-
-  mkNeovimModule = {
-    path,
-    ignoredPaths ? [./plugins/sources/default.nix],
-  }:
-    filter (hasSuffix ".nix") (
-      map toString (
-        filter (path: path != ./default.nix && !elem path ignoredPaths) (listFilesRecursive path)
-      )
-    );
-
-  nvf = inputs.neovim-flake;
-in {
-  imports = concatLists [
-    # neovim-flake home-manager module
-    [nvf.homeManagerModules.default]
-
-    # construct this entire directory as a module
-    # which means all default.nix files will be imported automatically
-    (mkNeovimModule {path = ./.;})
+}: {
+  imports = [
+    inputs.nvf.homeManagerModules.default
   ];
+
+  programs.neovim-flake = {
+    enable = true;
+    settings = {
+      vim = {
+        useSystemClipboard = true;
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+        };
+      };
+
+      autocomplete = {
+        enable = true;
+        mappings.scrollDocsUp = "<C-b>";
+      };
+
+      binds = {
+        cheatsheet.enable = true;
+        whichKey.enable = true;
+      };
+
+      comments.comment-nvim.enable = true;
+      debugger.nvim-dap = {
+        enable = true;
+        ui.enable = true;
+      };
+
+      autopairs.enable = true;
+
+      statusline.lualine = {
+        enable = true;
+      };
+      dashboard.startify = {
+        enable = true;
+      };
+
+      notify.nvim-notify.enable = true;
+
+      telescope.enable = true;
+      terminal.toggleterm = {
+        enable = true;
+        lazygit.enable = true;
+      };
+
+      ui = {
+        noice.enable = true;
+      };
+
+      git.enable = true;
+
+      languages = {
+	      nix = {
+          enable = true;
+          lsp.enable = true;
+          extraDiagnostics.enable = true;
+          format.enable = true;
+          tressitter.enable = true;
+        };
+
+        rust = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+        };
+
+        clang = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+          dap.enable = true;
+          cHeader.enable = true;
+        };
+
+	      ts = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+          format.enable = true;
+        };
+
+	      python = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+          format.enable = true;
+        };
+
+        markdown = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+          format.enable = true;
+        };
+
+	      html = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+          format.enable = true;
+        };
+
+        lua = {
+          enable = true;
+          lsp.enable = true;
+          tressitter.enable = true;
+          format.enable = true;
+        };
+
+        enableFormat = true;
+        enableLSP = true;
+        enableTreesitter = true;
+      };
+    };
+  };
 }
