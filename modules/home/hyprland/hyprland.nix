9982d4b4124d0db536905f4dcbbe0e9ebba421fc
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-06-05 00:14:13 +0200

    fix: fix typo 2

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -176,92 +176,92 @@ in {
       extraConfig = ''
       '';
     };
-  };
-  programs.anyrun = {
-    enable = true;
-    config = {
-      plugins = with inputs.anyrun.packages; [
-        applications
-        rink
-        translate
-        randr
-        shell
-        symbols
-        translate
+    programs.anyrun = {
+      enable = true;
+      config = {
+        plugins = with inputs.anyrun.packages; [
+          applications
+          rink
+          translate
+          randr
+          shell
+          symbols
+          translate
 
-        inputs.anyrun-nixos-options.packages.default
-      ];
+          inputs.anyrun-nixos-options.packages.default
+        ];
 
-      # the x coordinate of the runner
-      #x.relative = 800;
-      # the y coordinate of the runner
-      #y.absolute = 500.0;
-      y.fraction = 0.02;
+        # the x coordinate of the runner
+        #x.relative = 800;
+        # the y coordinate of the runner
+        #y.absolute = 500.0;
+        y.fraction = 0.02;
 
-      # Hide match and plugin info icons
-      hideIcons = false;
+        # Hide match and plugin info icons
+        hideIcons = false;
 
-      # ignore exclusive zones, i.e. Waybar
-      ignoreExclusiveZones = false;
+        # ignore exclusive zones, i.e. Waybar
+        ignoreExclusiveZones = false;
 
-      # Layer shell layer: Background, Bottom, Top, Overlay
-      layer = "overlay";
+        # Layer shell layer: Background, Bottom, Top, Overlay
+        layer = "overlay";
 
-      # Hide the plugin info panel
-      hidePluginInfo = false;
+        # Hide the plugin info panel
+        hidePluginInfo = false;
 
-      # Close window when a click outside the main box is received
-      closeOnClick = false;
+        # Close window when a click outside the main box is received
+        closeOnClick = false;
 
-      # Show search results immediately when Anyrun starts
-      showResultsImmediately = false;
+        # Show search results immediately when Anyrun starts
+        showResultsImmediately = false;
 
-      # Limit amount of entries shown in total
-      maxEntries = 10;
-    };
+        # Limit amount of entries shown in total
+        maxEntries = 10;
+      };
 
-    extraConfigFiles = {
-      "applications.ron".text = ''
-        Config(
-          // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
-          desktop_actions: true,
-          max_entries: 10,
-          // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
-          // to determine what terminal to use.
-          terminal: Some("foot"),
-        )
-      '';
+      extraConfigFiles = {
+        "applications.ron".text = ''
+          Config(
+            // Also show the Desktop Actions defined in the desktop files, e.g. "New Window" from LibreWolf
+            desktop_actions: true,
+            max_entries: 10,
+            // The terminal used for running terminal based desktop entries, if left as `None` a static list of terminals is used
+            // to determine what terminal to use.
+            terminal: Some("foot"),
+          )
+        '';
 
-      "randr.ron".text = ''
-        Config(
-          prefix: ":ra",
-          max_entries: 5,
-        )
-      '';
+        "randr.ron".text = ''
+          Config(
+            prefix: ":ra",
+            max_entries: 5,
+          )
+        '';
 
-      "symbols.ron".text = ''
-        Config(
-          // The prefix that the search needs to begin with to yield symbol results
-          prefix: ":sy",
+        "symbols.ron".text = ''
+          Config(
+            // The prefix that the search needs to begin with to yield symbol results
+            prefix: ":sy",
 
-          // Custom user defined symbols to be included along the unicode symbols
-          symbols: {
-            // "name": "text to be copied"
-            "shrug": "¯\\_(ツ)_/¯",
-          },
+            // Custom user defined symbols to be included along the unicode symbols
+            symbols: {
+              // "name": "text to be copied"
+              "shrug": "¯\\_(ツ)_/¯",
+            },
 
-          // The number of entries to be displayed
-          max_entries: 5,
-        )
-      '';
+            // The number of entries to be displayed
+            max_entries: 5,
+          )
+        '';
 
-      "translate.ron".text = ''
-        Config(
-          prefix: ":tr",
-          language_delimiter: ">",
-          max_entries: 3,
-        )
-      '';
+        "translate.ron".text = ''
+          Config(
+            prefix: ":tr",
+            language_delimiter: ">",
+            max_entries: 3,
+          )
+        '';
+      };
     };
   };
 }
